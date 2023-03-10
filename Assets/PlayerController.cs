f15ec693fe8da833e3f968209de35e4ca893c29c
Jamie Sullivan-Phillips <jamiesullphill@gmail.com>
2023-03-10 10:34:12 -0800

    Network aware player controller base

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerController : NetworkBehaviour
 {
-    
+
+    public override void OnNetworkSpawn()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Update()
     {
+        if (!IsSpawned)
+        {
+            return;
+        }
+        if (!IsOwner)
+        {
+            return;
+        }
         Vector3 moveDir = new Vector3(0, 0, 0);
         if(Input.GetKey(KeyCode.W))
         {
